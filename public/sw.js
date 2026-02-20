a6c68c36dba2cbeb32f7d8a63bf5b6581443a35a
Dr Asif Rao <mail@asifrao.com>
2026-02-20 23:29:08 +0000

    UI Redesign v4.5.0: Absolute Bottom Flush and FAB Fixes

--- public/sw.js
+++ public/sw.js
@@ -1,6 +1,6 @@
 // Service Worker for Jarvis PWA
 
-const CACHE_NAME = 'openclaw-pwa-v120';
+const CACHE_NAME = 'openclaw-pwa-v130';
 const SHELL_FILES = ['/pwa/', '/pwa/index.html'];
 
 // Badge count tracker
